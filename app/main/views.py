ee1bf8d91b47f092ab040041219ed46e6ebaf554
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-15 13:48:09 +0300

    Correct the spacing

--- app/main/views.py
+++ app/main/views.py
@@ -18,11 +18,8 @@ def index():
         new_sub = Subscribers(email = request.form.get("subscriber"))
         db.session.add(new_sub)
         db.session.commit()
-        welcome_message("Thank you for subscribing to the CM blog", 
-                        "email/welcome", new_sub.email)
-    return render_template("index.html",
-                            posts = posts,
-                            quote = quote)
+        welcome_message("Thank you for subscribing to the CM blog", "email/welcome", new_sub.email)
+    return render_template("index.html",posts = posts,quote = quote)
 
 @main.route("/post/<int:id>", methods = ["POST", "GET"])
 def post(id):
@@ -38,18 +35,11 @@ def post(id):
         comment_form.alias.data = ""
         if current_user.is_authenticated:
             comment_alias = current_user.username
-        new_comment = Comment(comment = comment, 
-                            comment_at = datetime.now(),
-                            comment_by = comment_alias,
-                            post_id = id)
+        new_comment = Comment(comment = comment, comment_at = datetime.now(), comment_by = comment_alias, post_id = id)
         new_comment.save_comment()
         return redirect(url_for("main.post", id = post.id))
 
-    return render_template("post.html",
-                            post = post,
-                            comments = comments,
-                            comment_form = comment_form,
-                            comment_count = comment_count)
+    return render_template("post.html",post = post,comments = comments,comment_form = comment_form,comment_count = comment_count)
 
 @main.route("/post/<int:id>/<int:comment_id>/delete")
 def delete_comment(id, comment_id):
@@ -84,9 +74,7 @@ def edit_post(id):
         db.session.commit()
         return redirect(url_for("main.post", id = post.id))
 
-    return render_template("edit_post.html", 
-                            post = post,
-                            edit_form = edit_form)
+    return render_template("edit_post.html", post = post,edit_form = edit_form)
 
 @main.route("/post/new", methods = ["POST", "GET"])
 @login_required
@@ -95,28 +83,17 @@ def new_post():
     if post_form.validate_on_submit():
         post_title = post_form.title.data
         post_form.title.data = ""
-        post_content = bleach.clean(post_form.post.data, 
-                                    tags = bleach.sanitizer.ALLOWED_TAGS + ["h1", "h2", "h3", "h4",
-                                                                            "h5", "h6", "p", "span",
-                                                                            "div", "br", "em", "strong"
-                                                                            "i", "blockquote", "hr", "a"
-                                                                            "ul", "ol", "li"])
+        post_content = bleach.clean(post_form.post.data, tags = bleach.sanitizer.ALLOWED_TAGS + ["h1", "h2", "h3", "h4","h5", "h6", "p", "span","div", "br", "em", "strong","i", "blockquote", "hr", "a","ul", "ol", "li"])
         post_form.post.data = ""
-        new_post = Post(post_title = post_title,
-                        post_content = post_content,
-                        posted_at = datetime.now(),
-                        post_by = current_user.username,
-                        user_id = current_user.id)
+        new_post = Post(post_title = post_title,post_content = post_content,posted_at = datetime.now(), post_by = current_user.username, user_id = current_user.id)
         new_post.save_post()
         subs = Subscribers.query.all()
         for sub in subs:
-            notification_message(post_title, 
-                            "email/notification", sub.email, new_post = new_post)
+            notification_message(post_title, "email/notification", sub.email, new_post = new_post)
             pass
         return redirect(url_for("main.post", id = new_post.id))
     
-    return render_template("new_post.html",
-                            post_form = post_form)
+    return render_template("new_post.html", post_form = post_form)
 
 @main.route("/profile/<int:id>", methods = ["POST", "GET"])
 def profile(id):
@@ -127,12 +104,9 @@ def profile(id):
         new_sub = Subscribers(email = request.form.get("subscriber"))
         db.session.add(new_sub)
         db.session.commit()
-        welcome_message("Thank you for subscribing to the CM blog", 
-                        "email/welcome", new_sub.email)
+        welcome_message("Thank you for subscribing to the CM blog", "email/welcome", new_sub.email)
 
-    return render_template("profile/profile.html",
-                            user = user,
-                            posts = posts)
+    return render_template("profile/profile.html",user = user, posts = posts)
 
 @main.route("/profile/<int:id>/<int:post_id>/delete")
 @login_required
@@ -158,6 +132,4 @@ def update_profile(id):
         db.session.commit()
         return redirect(url_for("main.profile", id = id))
     
-    return render_template("profile/update.html",
-                            user = user,
-                            form = form)
+    return render_template("profile/update.html", user = user, form = form)
