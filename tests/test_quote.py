8f2c36300b8ca49319f6ee9902a763d837271c13
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-15 08:02:06 +0300

    Create a unit tests for the random quotes

---
+++ tests/test_quote.py
@@ -0,0 +1,16 @@
+import unittest
+from app.models import Quote
+
+class TestQuote(unittest.TestCase):
+    def setUp(self):
+        """
+        Set up method that will run before every Test
+        """
+        self.random_quote = Quote("Cotech Levy", "Hello World")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.random_quote, Quote))
+
+    def test_init(self):
+        self.assertEqual(self.random_quote.author, "Cotech Levy")
+        self.assertEqual(self.random_quote.quote,"Hello World")
