0620b074012cb30eed63bfa63f471e00882754d3
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-15 11:40:41 +0300

    Add the sqlalchemy configuration to the config file

---
+++ config.py
@@ -0,0 +1,33 @@
+import os 
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    UPLOADED_PHOTOS_DEST = "app/static/photos"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # email configurations
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class ProdConfig(Config):
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI_TEST")
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    DEBUG = True
+
+
+config_options = {
+    "development": DevConfig,
+    "production": ProdConfig,
+    "test": TestConfig
+}
