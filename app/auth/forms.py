4194e93c94f9f3176c0a15fc887fdbea67385a30
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-15 11:35:53 +0300

    Correct the spacing

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import (StringField, PasswordField, SubmitField, 
-                    ValidationError, BooleanField)
+from wtforms import (StringField, PasswordField, SubmitField, ValidationError, BooleanField)
 from wtforms.validators import DataRequired, Email, EqualTo
 from ..models import User
 
@@ -9,8 +8,7 @@ class SignUpForm(FlaskForm):
     last_name = StringField("Your Last Name", validators=[DataRequired()])
     username = StringField("Your Username", validators=[DataRequired()])
     email = StringField("Your Email Address", validators=[DataRequired(), Email()])
-    password = PasswordField("Password", validators=[DataRequired(), 
-                             EqualTo("password_confirm", message = "Passwords must match")])
+    password = PasswordField("Password", validators=[DataRequired(), EqualTo("password_confirm", message = "Passwords must match")])
     password_confirm = PasswordField("Confirm Password", validators=[DataRequired()])
     submit = SubmitField("Sign Up")
 
