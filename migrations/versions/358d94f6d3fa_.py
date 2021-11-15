a80555a85d7f7dd22f8c19f84819d83eb1bef801
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-15 20:35:00 +0300

    Initial migration

---
+++ migrations/versions/358d94f6d3fa_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 358d94f6d3fa
+Revises: 
+Create Date: 2021-11-15 20:24:41.596410
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '358d94f6d3fa'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
