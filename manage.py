276835b01ea0a58816d9841dae92775c6826d5e4
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-15 16:12:33 +0300

    Activate production environment

--- manage.py
+++ manage.py
@@ -3,7 +3,8 @@ from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from app.models import User, Post, Comment, PostLike
 
-app = create_app("development")
+app = create_app("production")
+#app = create_app("development")
 manager = Manager(app)
 manager.add_command("server", Server)
 
