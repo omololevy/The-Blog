68977d77367186a63e564f27c90fbc908d626d5b
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-16 05:16:17 +0300

    sql config

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
 
 class ProdConfig(Config):
     
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql://kbrdrqaqcaqxgh:4b701cf3e3c72bd5704021ac5096b74121ef8f21d548812f2bf337a691fa81ed@ec2-52-201-195-11.compute-1.amazonaws.com:5432/dfuhcscf9iu9bf'
 
 class TestConfig(Config):
     
