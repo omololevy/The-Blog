1b348189c906115ccb97500f2b61ff6e3d1cf3ff
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-18 09:27:33 +0300

    Update the views content

--- app/auth/views.py
+++ app/auth/views.py
@@ -10,34 +10,33 @@ from .. import db
 @auth.route("/signup", methods = ["GET", "POST"])
 
 def register():
-    signup_form = SignUpForm(csrf_enabled=False)
+    '''Function to register the user and commit it to the db'''
+    signup_form = SignUpForm()
     if signup_form.validate_on_submit():
         user = User(first_name = signup_form.first_name.data,
                     last_name = signup_form.last_name.data,  
                     username = signup_form.username.data, 
                     email = signup_form.email.data,
                     password = signup_form.password.data)
-        db.session.add(user)
-        db.session.commit()
+        #db.session.add(user)
+        #db.session.commit()
 
-        # mail_message("Welcome to 60 seconds",
-        #              "email/welcome", user.email, user = user)
+        # mail_message("Welcome to 60 seconds",   "email/welcome", user.email, user = user)
+        
         return redirect(url_for("auth.login"))
     title = "Sign Up The-Blog"
-    return render_template("auth/signup.html", 
-                            signup_form = signup_form,
-                            title = title)
+    return render_template("auth/signup.html",signup_form = signup_form, title = title)
 
 @auth.route("/login", methods = ["GET", "POST"])
 def login():
-    login_form = LoginForm(csrf_enabled=False)
+    login_form = LoginForm()
     if login_form.validate_on_submit():
         user = User.query.filter_by(email = login_form.email.data).first()
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user, login_form.remember.data)
             return redirect(request.args.get("next") or url_for("main.index"))
 
-        flash("Invalid Username or Password")
+        flash("Invalid Username or Password or the user exists already")
     
     title = "Login to The-Blog"
     return render_template("auth/login.html",
