3727cadfd180d57dd5d0542835cda855a6e226c6
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-18 09:34:37 +0300

    Update the unit tests to test the cases

---
+++ tests/test_comment.py
@@ -0,0 +1,22 @@
+import unittest
+from app.models import Post, User, Comment
+
+class TestPost(unittest.TestCase):
+    
+    def setUp(self):
+        self.user_dev = User(first_name = "Levy",
+                                last_name = "Cotech",
+                                username = "cotechlevy",
+                                password = "1234",
+                                email = "cotechlevy@mail.com")
+        self.new_post = Post(post_title = " Title",
+                            post_content = "I can see you there",
+                            user_id = self.user_dev.id)
+        self.new_comment = Comment(comment = "Nice job",
+                                    post_id = self.new_post.id,
+                                    user_id = self.user_dev.id)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user_dev, User))
+        self.assertTrue(isinstance(self.new_post, Post))
+        self.assertTrue(isinstance(self.new_comment, Comment))
---
+++ tests/test_quote.py
@@ -0,0 +1,16 @@
+import unittest
+from app.models import Quote
+
+class TestQuote(unittest.TestCase):
+    def setUp(self):
+        """
+        Set up method that will run before every Test
+        """
+        self.random_quote = Quote("Levy Cotech", "Hello there")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.random_quote, Quote))
+
+    def test_init(self):
+        self.assertEqual(self.random_quote.author, "Levy Cotech")
+        self.assertEqual(self.random_quote.quote,"Hello there")
